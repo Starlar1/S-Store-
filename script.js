34b4a8252bb5c64825325844fc925f6ae1aee2c9
Starlar1 <vtubeychan@gmail.com>
2026-03-07 19:02:38 +0630

    Fix formatting issues in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<meta name='viewport' content='width=device-width, initial-scale=1'/><script>(function () {
+(function () {
   // ---------- Supabase Setup ----------
   const supabaseClients = [
     supabase.createClient(
